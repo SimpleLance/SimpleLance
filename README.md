fb615392f218f0df76722cd6ed80d01797393119
Mark Railton <mark@markrailton.com>
2014-06-14 20:59:42 +0400

    Added SQL file and updated readme

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ SimpleLance
 ===========
 
 A simple customer management / invoicing / project tracking tool built for freelancers.
+
+Install by copying files to web root and importing sql file. Make sure to edit config.php to suit
+
+At present only basic user management is working.
+
+2 Accounts exist, 1 admin and 1 cust.
+
+admin - email = admin@simplelance.com / password = admin <br>
+customer - email = customer@simplelance.com / password = customer
+
+Development is ongoing and updates will be released as regular as possible.
+
+If you have a feature you would like to see added, please create an issue or PR
---
+++ simplelance_2014-06-14.sql
@@ -0,0 +1,46 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: localhost (MySQL 5.6.17)
+# Database: simplelance
+# Generation Time: 2014-06-14 16:55:10 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table projects
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `projects`;
+
+CREATE TABLE `projects` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `description` text,
+  `created_on` datetime DEFAULT NULL,
+  `customer` int(11) DEFAULT NULL,
+  `status` varchar(16) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
