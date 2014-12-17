50a25a6dd8a2bed45f1d09fa0a2746cdc9c1ba03
Svpernova09 <joe@joeferguson.me>
2014-12-17 10:28:45 -0600

    Change default database info to support vagrant out of the box

--- includes/config.php
+++ includes/config.php
@@ -8,8 +8,8 @@ define('SITE_URL', 'simplelance.dev');
 // define database settings
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'simplelance');
+define('DB_PASSWORD', 'vagrant');
+define('DB_NAME', 'vagrant');
 
 // define currency
 define('CURRNAME', 'Euro');
