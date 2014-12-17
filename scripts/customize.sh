fb7c1e3fae2aa77f7cdeea70912af919a4c770ad
Svpernova09 <joe@joeferguson.me>
2014-12-17 12:52:05 -0600

    Fix broken link to vendor/bin

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -6,6 +6,6 @@ echo "Added scree phantomjs to crontab"
 screen -S server -d -m phantomjs --webdriver=4444
 /usr/local/bin/composer self-update
 echo "" >> /home/vagrant/.bashrc
-echo "PATH=$PATH:/home/vagrant/vendor/bin" >> /home/vagrant/.bashrc
+echo "PATH=$PATH:/home/vagrant/simplelance.dev/vendor/bin" >> /home/vagrant/.bashrc
 echo "export PATH" >> /home/vagrant/.bashrc
 echo Done!
