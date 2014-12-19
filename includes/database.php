4ba56e487a9e328175dc1c3a65574c3c619fcf17
Svpernova09 <joe@joeferguson.me>
2014-12-19 08:10:11 -0600

    Like ripping off a bandaid. Removing the old legacy version of the app to make way for laravel

--- includes/database.php
+++
@@ -1,4 +0,0 @@
-<?php
-// setup db connection
-$db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- tests/acceptance/_bootstrap.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Here you can initialize variables that will be available to your tests
-require_once 'commons/TestCommonsUser.php';
-require_once 'commons/TestCommonsAdmin.php';
