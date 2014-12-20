d89ef4dffe412a15481a16f08814aff3bd3a96d2
Svpernova09 <joe@joeferguson.me>
2014-12-19 23:33:07 -0600

    Break sidebar menus into their own templates

---
+++ app/views/layouts/menus/admin.blade.php
@@ -0,0 +1,54 @@
+<li class="sidebar-search">
+    <div class="input-group custom-search-form">
+        <input type="text" class="form-control" placeholder="Search...">
+                                <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                            </span>
+    </div>
+</li>
+<li>
+    <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> User Management<span class="fa arrow"></span></a>
+    <ul class="nav nav-second-level">
+        <li>
+            <a href="/users">View Users</a>
+        </li>
+        <li>
+            <a href="/users/register.php">Add User</a>
+        </li>
+    </ul>
+</li>
+<li>
+    <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Project Tracker<span class="fa arrow"></span></a>
+    <ul class="nav nav-second-level">
+        <li>
+            <a href="/projects">View Projects</a>
+        </li>
+        <li>
+            <a href="/projects/new.php">Add Project</a>
+        </li>
+    </ul>
+</li>
+<li>
+    <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Support Tickets<span class="fa arrow"></span></a>
+    <ul class="nav nav-second-level">
+        <li>
+            <a href="/support">View Tickets</a>
+        </li>
+        <li>
+            <a href="/support/new.php">Add Ticket</a>
+        </li>
+    </ul>
+</li>
+<li>
+    <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Invoices<span class="fa arrow"></span></a>
+    <ul class="nav nav-second-level">
+        <li>
+            <a href="/billing">View Invoices</a>
+        </li>
+        <li>
+            <a href="/billing/new.php">Create Invoice</a>
+        </li>
+    </ul>
+</li>
---
+++ app/views/layouts/menus/user.blade.php
@@ -0,0 +1,12 @@
+<li>
+    <a href="/users/profile"><i class="fa fa-user fa-fw"></i> User Profile</a>
+</li>
+<li>
+    <a href="/projects"><i class="fa fa-user fa-fw"></i> Projects</a>
+</li>
+<li>
+    <a href="/support"><i class="fa fa-user fa-fw"></i> Support Tickets</a>
+</li>
+<li>
+    <a href="/billing"><i class="fa fa-user fa-fw"></i> Invoices</a>
+</li>
