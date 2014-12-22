9646ee9a9be62ceaece515370de9289948f4d94a
Mark Railton <marksrailton@gmail.com>
2014-12-22 17:48:20 +0400

    Refactored tests to have common includes for login admin and user

--- tests/acceptance/Login/AdminLoginCept.php
+++ tests/acceptance/Login/AdminLoginCept.php
@@ -1,9 +1,5 @@
 <?php
 $I = new AcceptanceTester($scenario);
 $I->wantTo('ensure that an admin can login');
-$I->amOnPage('/');
-$I->click('Log In');
-$I->fillField('email', 'admin@admin.com');
-$I->fillField('password', 'simplelance');
-$I->click('Sign In');
+TestCommonAdmin::logMeIn($I);
 $I->see('Hello admin!');
