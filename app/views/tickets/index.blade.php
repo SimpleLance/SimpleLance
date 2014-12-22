748fb47ed2ba081415a79cf07bbea520d6ecce54
Svpernova09 <joe@joeferguson.me>
2014-12-22 07:30:02 -0600

    Display the support tickets on the index view

--- app/views/tickets/index.blade.php
+++ app/views/tickets/index.blade.php
@@ -1,6 +1,37 @@
 @extends('layouts.default')
 
 @section('content')
-    TODO: index ticket listings
+    <h1>Support Tickets</h1>
+
+    <table class="table table-striped table-hover">
+        <thead>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Priority</th>
+            <th>Owner</th>
+        </thead>
+        <tbody>
+        @foreach ($tickets as $ticket)
+            <tr>
+                <td>
+                    <a href="{{ action('TicketsController@show', array($ticket->id)) }}">
+                        {{ $ticket->title }}
+                    </a>
+                </td>
+                <td>
+                    {{ $ticket->description }}
+                </td>
+                <td>
+                    {{ $ticket->priority_id }}
+                    {{--TODO: Swap ID with Priority Value--}}
+                </td>
+                <td>
+                    {{ $ticket->owner_id }}
+                    {{--TODO: Swap ID with User Value--}}
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 @stop
 
