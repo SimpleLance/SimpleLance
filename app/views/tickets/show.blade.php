fba6bdce432c618a5df7733d16d8fea19c0981f7
Svpernova09 <joe@joeferguson.me>
2014-12-22 07:45:23 -0600

    Add content to show template and add test for show view

--- app/views/tickets/show.blade.php
+++ app/views/tickets/show.blade.php
@@ -1,6 +1,21 @@
 @extends('layouts.default')
 
 @section('content')
-    TODO: Show ticket detail
+    <h4>Support Ticket</h4>
+
+    <div class="well clearfix">
+        <div class="col-md-8">
+            <p><strong>Title</strong>: {{ $ticket->title }}</p>
+            <p><strong>Description</strong>: {{ $ticket->description }}</p>
+            <p><strong>Priority</strong>: {{ $ticket->priority_id }}</p>
+            <p><strong>Owner</strong>: {{ $ticket->owner_id }}</p>
+
+        </div>
+        <div class="col-md-4">
+            <p><em>Ticket created: {{ $ticket->created_at }}</em></p>
+            <p><em>Last Updated: {{ $ticket->updated_at }}</em></p>
+            <button class="btn btn-primary" onClick="location.href='{{ action('TicketsController@edit', array($ticket->id)) }}'">Edit Ticket</button>
+        </div>
+    </div>
 @stop
 
