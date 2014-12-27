812eb34ba7484b998a2dd8326999278d4afc292a
Mark Railton <marksrailton@gmail.com>
2014-12-27 22:11:33 +0400

    Updated UI on projects list to match with tickets

--- app/views/projects/index.blade.php
+++ app/views/projects/index.blade.php
@@ -1,37 +1,37 @@
 @extends('layouts.default')
 
 @section('content')
-    @if (Sentry::check())
-        <div class="row col-md-9 col-md-offset-1">
-            <h2>Current Open Projects</h2>
-            <br>
-            <table class="table">
-                <thead>
+    <h1>Current Projects</h1>
+
+    <table class="table table-striped table-hover">
+        <thead>
+        <th>Title</th>
+        <th>Description</th>
+        <th>Status</th>
+        <th>Owner</th>
+        </thead>
+        <tbody>
+        @foreach ($projects as $project)
+            @if (Sentry::inGroup(Sentry::findGroupByName('Admins')) || $project->owner_id == Sentry::getUser()->id)
                 <tr>
-                    <th>Title</th>
-                    <th>Description</th>
-                    <th>Owner</th>
-                    <th>Status</th>
-                    <th>Last Updated</th>
+                    <td>
+                        <a href="{{ action('ProjectsController@show', array($project->id)) }}">
+                            {{ $project->title }}
+                        </a>
+                    </td>
+                    <td>
+                        {{ $project->description }}
+                    </td>
+                    <td>
+                        {{ $project->status->title }}
+                    </td>
+                    <td>
+                        {{ $project->owner->username }}
+                    </td>
                 </tr>
-                </thead>
-                <tbody>
-                @foreach($projects as $project)
-                    @if (Sentry::inGroup(Sentry::findGroupByName('Admins')) || $project->owner_id == Sentry::getUser()->id)
-                        <tr>
-                            <td><a href="{{ action('ProjectsController@show', array($project->id)) }}">
-                                    {{ $project->title }}
-                                </a></td>
-                            <td>{{ $project->description }}</td>
-                            <td>{{ $project->owner->username }}</td>
-                            <td>{{ $project->status->title }}</td>
-                            <td>{{ date('jS F Y', strtotime($project->updated_at)) }}</td>
-                        </tr>
-                    @endif
-                @endforeach
-                </tbody>
-            </table>
-        </div>
-    @endif
+            @endif
+        @endforeach
+        </tbody>
+    </table>
 @stop
 
