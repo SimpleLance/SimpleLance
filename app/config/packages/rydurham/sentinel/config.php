3a0436f4d4a09ebf9bd4236eec1d86ce9ec2091b
Svpernova09 <joe@joeferguson.me>
2015-01-12 08:28:03 -0600

    Add user profile fields to sentinel

--- app/config/packages/rydurham/sentinel/config.php
+++ app/config/packages/rydurham/sentinel/config.php
@@ -155,6 +155,15 @@ return array(
 
     'additional_user_fields' => array(
         //'field_name' => 'validation'
+        'first_name' => 'required',
+        'last_name' => 'required',
+        'address' => 'required',
+        'address2' => 'required',
+        'city' => 'required',
+        'state' => 'required',
+        'post_code' => 'required',
+        'country' => 'required',
+        'phone' => 'required',
     ),
 
     /*
