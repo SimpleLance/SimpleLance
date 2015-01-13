abc11db6ace99a13cb61ed85a4098925175b5edb
Svpernova09 <joe@joeferguson.me>
2015-01-13 08:59:21 -0600

    Add project stats

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -3,10 +3,12 @@
 class DashboardController extends BaseController {
 
 	protected $ticket;
+	protected $project;
 
-	public function __construct(Ticket $ticket)
+	public function __construct(Ticket $ticket, Project $project)
 	{
 		$this->ticket = $ticket;
+		$this->project = $project;
 	}
 
 	public function index()
@@ -15,11 +17,14 @@ class DashboardController extends BaseController {
 		{
 			$openTickets = $this->ticket->getOpenTickets();
 			$inProgressTickets = $this->ticket->getInProgressTickets();
-
+			$openProjects = $this->project->getOpenProjects();
+			$inProgressProjects = $this->project->getInProgressProjects();
 
 			return View::make('dashboard.admin')
 				->with('openTickets', $openTickets)
-				->with('inProgressTickets', $inProgressTickets);
+				->with('inProgressTickets', $inProgressTickets)
+				->with('openProjects', $openProjects)
+				->with('inProgressProjects', $inProgressProjects);
 		} else {
 
 			return View::make('dashboard.user');
