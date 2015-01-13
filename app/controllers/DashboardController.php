c10f4339ff7a4c11cc879a1ea420ba38a48c23d0
Svpernova09 <joe@joeferguson.me>
2015-01-13 08:26:18 -0600

    Get support tickets by statuses and pass to the admin view

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -2,15 +2,29 @@
 
 class DashboardController extends BaseController {
 
+	protected $ticket;
+
+	public function __construct(Ticket $ticket)
+	{
+		$this->ticket = $ticket;
+	}
+
 	public function index()
 	{
 		if (Sentry::inGroup(Sentry::findGroupByName('Admins')))
 		{
+			$openTickets = $this->ticket->getOpenTickets();
+			$inProgressTickets = $this->ticket->getInProgressTickets();
 
-			return View::make('dashboard.admin');
+
+			return View::make('dashboard.admin')
+				->with('openTickets', $openTickets)
+				->with('inProgressTickets', $inProgressTickets);
 		} else {
 
 			return View::make('dashboard.user');
 		}
 	}
+
+
 }
