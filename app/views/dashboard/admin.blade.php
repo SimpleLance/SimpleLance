ecb0bc7c19338f275a470c1f1a0599afaaf8fc3f
Svpernova09 <joe@joeferguson.me>
2015-01-13 08:26:33 -0600

    Boilerplate for admin dashboard data

--- app/views/dashboard/admin.blade.php
+++ app/views/dashboard/admin.blade.php
@@ -2,5 +2,10 @@
 
 @section('content')
     Admin Dashboard
+
+    Open Tickets: {{ count($openTickets) }} / In Progress Tickets: {{ count($inProgressTickets) }} <br />
+    Count of number of open/in-progress projects (should show split of open/in-progress) <br />
+    Count of number of open invoices, also including total $ amount for all open invoices <br />
+    Count of number of overdue invoices, also including total $ amount for all overdue invoices <br />
 @stop
 
