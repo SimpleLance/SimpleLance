2eab2560ff4eb6de77ba783ba3170cb140f14584
Svpernova09 <joe@joeferguson.me>
2015-01-14 08:12:52 -0600

    Use paragraphs instead of BRs

--- app/views/dashboard/admin.blade.php
+++ app/views/dashboard/admin.blade.php
@@ -4,18 +4,25 @@
     <h1>Admin Dashboard</h1>
 
     <h3>Support Tickets</h3>
-    Open Tickets: {{ count($data['openTickets']) }} /
-    In Progress Tickets: {{ count($data['inProgressTickets']) }} <br />
+    <p>
+        Open Tickets: {{ count($data['openTickets']) }} /
+        In Progress Tickets: {{ count($data['inProgressTickets']) }}
+    </p>
 
     <h3>Projects</h3>
-    Open Projects: {{ count($data['openProjects']) }} /
-    In Progress Projects: {{ count($data['inProgressProjects']) }} <br />
+    <p>
+        Open Projects: {{ count($data['openProjects']) }} /
+        In Progress Projects: {{ count($data['inProgressProjects']) }}
+    </p>
 
     <h3>Invoices:</h3>
-    Open Invoices: {{ count($data['openInvoices']) }} /
-    Total Open Amount: {{ $data['openInvoicesAmount'] }}<br />
-
-    Overdue Invoices: {{ count($data['overdueInvoices']) }} /
-    Total Overdue Amount: {{ $data['overdueInvoicesAmount'] }}<br />
+    <p>
+        Open Invoices: {{ count($data['openInvoices']) }} /
+        Total Open Amount: {{ $data['openInvoicesAmount'] }}
+    </p>
+    <p>
+        Overdue Invoices: {{ count($data['overdueInvoices']) }} /
+        Total Overdue Amount: {{ $data['overdueInvoicesAmount'] }}
+    </p>
 @stop
 
