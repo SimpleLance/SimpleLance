d3233ddb5c24c3aa9b7335f10be437b06d969ac6
Svpernova09 <joe@joeferguson.me>
2015-01-14 07:18:09 -0600

    Add methods and data to the admin dashboard

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -4,27 +4,33 @@ class DashboardController extends BaseController {
 
 	protected $ticket;
 	protected $project;
+	protected $invoice;
 
-	public function __construct(Ticket $ticket, Project $project)
+	public function __construct(
+		Ticket $ticket,
+		Project $project,
+		Invoice $invoice
+	)
 	{
 		$this->ticket = $ticket;
 		$this->project = $project;
+		$this->invoice = $invoice;
 	}
 
 	public function index()
 	{
 		if (Sentry::inGroup(Sentry::findGroupByName('Admins')))
 		{
-			$openTickets = $this->ticket->getOpenTickets();
-			$inProgressTickets = $this->ticket->getInProgressTickets();
-			$openProjects = $this->project->getOpenProjects();
-			$inProgressProjects = $this->project->getInProgressProjects();
+			$data['openTickets'] = $this->ticket->getOpenTickets();
+			$data['inProgressTickets'] = $this->ticket->getInProgressTickets();
+			$data['openProjects'] = $this->project->getOpenProjects();
+			$data['inProgressProjects'] = $this->project->getInProgressProjects();
+			$data['openInvoices'] = $this->invoice->getOpenInvoices();
+			$data['openInvoicesAmount'] = $this->invoice->getOpenInvoicesTotalAmount();
+			$data['overdueInvoices'] = $this->invoice->getOverdueInvoices();
+			$data['overdueInvoicesAmount'] = $this->invoice->getOverdueInvoicesTotalAmount();
 
-			return View::make('dashboard.admin')
-				->with('openTickets', $openTickets)
-				->with('inProgressTickets', $inProgressTickets)
-				->with('openProjects', $openProjects)
-				->with('inProgressProjects', $inProgressProjects);
+			return View::make('dashboard.admin')->with('data', $data);
 		} else {
 
 			return View::make('dashboard.user');
--- app/models/Invoice.php
+++ app/models/Invoice.php
@@ -18,4 +18,47 @@ class Invoice extends \Eloquent {
 
 		return $this->belongsTo('Status');
 	}
+
+	public function getOpenInvoices() {
+
+		return Invoice::where('status_id', '=', '1')->get();
+	}
+
+	public function getOpenInvoicesTotalAmount() {
+		$invoices = $this->getOpenInvoices();
+
+		$totalAmount = 0;
+
+		foreach ($invoices as $invoice) {
+			$totalAmount = $totalAmount + $invoice->amount;
+		}
+
+		return $totalAmount;
+	}
+
+	public function getOverdueInvoices() {
+		$invoices = $this->getOpenInvoices();
+		$overdueInvoices = [];
+		$today = date("Y-m-d");
+
+		foreach ($invoices as $invoice) {
+			if($invoice->due < $today){
+				$overdueInvoices[] = $invoice;
+			}
+		}
+
+		return $overdueInvoices;
+	}
+
+	public function getOverdueInvoicesTotalAmount() {
+		$invoices = $this->getOverdueInvoices();
+
+		$totalAmount = 0;
+
+		foreach ($invoices as $invoice) {
+			$totalAmount = $totalAmount + $invoice->amount;
+		}
+
+		return $totalAmount;
+	}
 }
