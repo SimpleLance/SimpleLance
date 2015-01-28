ae71b29a719734ce2903400b10d7c4c669f2316f
Svpernova09 <joe@joeferguson.me>
2015-01-28 09:13:17 -0600

    Upgrade to Homestead 2.0

--- scripts/simplelance.rb
+++ scripts/simplelance.rb
@@ -1,28 +1,38 @@
-class SL
-  def SL.configure(config, settings)
+class Homestead
+  def Homestead.configure(config, settings)
     # Configure The Box
     config.vm.box = "laravel/homestead"
     config.vm.hostname = "homestead"
 
     # Configure A Private Network IP
-    config.vm.network :private_network, ip: settings["ip"] ||= "192.168.64.10"
+    config.vm.network :private_network, ip: settings["ip"] ||= "192.168.10.10"
 
     # Configure A Few VirtualBox Settings
     config.vm.provider "virtualbox" do |vb|
+      vb.name = 'SimpleLance'
       vb.customize ["modifyvm", :id, "--memory", settings["memory"] ||= "2048"]
       vb.customize ["modifyvm", :id, "--cpus", settings["cpus"] ||= "1"]
       vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
       vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+      vb.customize ["modifyvm", :id, "--ostype", "Ubuntu_64"]
     end
 
     # Configure Port Forwarding To The Box
     config.vm.network "forwarded_port", guest: 80, host: 8000
+    config.vm.network "forwarded_port", guest: 443, host: 44300
     config.vm.network "forwarded_port", guest: 3306, host: 33060
     config.vm.network "forwarded_port", guest: 5432, host: 54320
 
+    # Add Custom Ports From Configuration
+    if settings.has_key?("ports")
+      settings["ports"].each do |port|
+        config.vm.network "forwarded_port", guest: port["guest"], host: port["host"], protocol: port["protocol"] ||= "tcp"
+      end
+    end
+
     # Configure The Public Key For SSH Access
     config.vm.provision "shell" do |s|
-      s.inline = "echo $1 | tee -a /home/vagrant/.ssh/authorized_keys"
+      s.inline = "echo $1 | grep -xq \"$1\" /home/vagrant/.ssh/authorized_keys || echo $1 | tee -a /home/vagrant/.ssh/authorized_keys"
       s.args = [File.read(File.expand_path(settings["authorize"]))]
     end
 
@@ -35,11 +45,6 @@ class SL
       end
     end
 
-    # Copy The Bash Aliases
-    config.vm.provision "shell" do |s|
-      s.inline = "cp /vagrant/aliases /home/vagrant/.bash_aliases"
-    end
-
     # Register All Of The Configured Shared Folders
     settings["folders"].each do |folder|
       config.vm.synced_folder folder["map"], folder["to"], type: folder["type"] ||= nil
@@ -48,19 +53,51 @@ class SL
     # Install All The Configured Nginx Sites
     settings["sites"].each do |site|
       config.vm.provision "shell" do |s|
-          s.inline = "bash /vagrant/scripts/serve.sh $1 $2"
-          s.args = [site["map"], site["to"]]
+          if (site.has_key?("hhvm") && site["hhvm"])
+            s.inline = "bash /home/vagrant/simplelance.dev/scripts/serve-hhvm.sh $1 \"$2\""
+            s.args = [site["map"], site["to"]]
+          else
+            s.inline = "bash /home/vagrant/simplelance.dev/scripts/serve.sh $1 \"$2\""
+            s.args = [site["map"], site["to"]]
+          end
       end
+    end
+
+    # Configure All Of The Configured Databases
+    settings["databases"].each do |db|
+        config.vm.provision "shell" do |s|
+            s.path = "./scripts/create-mysql.sh"
+            s.args = [db]
+        end
+
+        config.vm.provision "shell" do |s|
+            s.path = "./scripts/create-postgres.sh"
+            s.args = [db]
+        end
     end
 
     # Configure All Of The Server Environment Variables
     if settings.has_key?("variables")
       settings["variables"].each do |var|
         config.vm.provision "shell" do |s|
-            s.inline = "echo \"\nenv[$1] = '$2'\" >> /etc/php5/fpm/php-fpm.conf && service php5-fpm restart"
+            s.inline = "echo \"\nenv[$1] = '$2'\" >> /etc/php5/fpm/php-fpm.conf"
+            s.args = [var["key"], var["value"]]
+        end
+
+        config.vm.provision "shell" do |s|
+            s.inline = "echo \"\n#Set Homestead environment variable\nexport $1=$2\" >> /home/vagrant/.profile"
             s.args = [var["key"], var["value"]]
         end
       end
+
+      config.vm.provision "shell" do |s|
+          s.inline = "service php5-fpm restart"
+      end
+    end
+
+    # Update Composer On Every Provision
+    config.vm.provision "shell" do |s|
+      s.inline = "/usr/local/bin/composer self-update"
     end
   end
 end
