7730ad92087422d7cb905627692972d0777cae6f
Bryan Robinson <bryanlrobinson@gmail.com>
2015-03-09 07:56:49 -0500

    Changed time format on detail templates

--- app/views/projects/show.blade.php
+++ app/views/projects/show.blade.php
@@ -4,7 +4,7 @@
     <h4>Project</h4>
 
     <div class="well clearfix">
-        <div class="col-md-8">
+        <div class="col-md-7">
             <p><strong>Title</strong>: {{ $project->title }}</p>
             <p><strong>Description</strong>: {{ $project->description }}</p>
             <p><strong>Status</strong>: {{ $project->status->title }}</p>
@@ -12,9 +12,10 @@
             <p><strong>Owner</strong>: {{ $project->owner->username }}</p>
 
         </div>
-        <div class="col-md-4">
-            <p><em>Project created: {{ $project->created_at }}</em></p>
-            <p><em>Last Updated: {{ $project->updated_at }}</em></p>
+        <div class="col-md-5">
+
+            <p><em>Project created: {{ date("D, F d Y",strtotime($project->created_at)) }}</em></p>
+            <p><em>Last Updated: {{ date("D, F d Y",strtotime($project->updated_at)) }} at {{ date("g:h a",strtotime($project->updated_at)) }}</em></p>
             <button id="edit-{{ $project->id }}" class="btn btn-primary" onClick="location.href='{{ action('ProjectsController@edit', array($project->id)) }}'">Edit Project</button>
         </div>
         <div class="col-md-2">
