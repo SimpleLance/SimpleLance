83ae30a59816e3cbf48b8417dc5fee16e695b77e
Joe Ferguson <joe@joeferguson.me>
2015-04-13 19:39:54 -0500

    Fix 404 page from enabling filters and check for explicit text for 404 message.

--- tests/acceptance/Pages/404WorksCept.php
+++ tests/acceptance/Pages/404WorksCept.php
@@ -1,7 +1,6 @@
 <?php
 $I = new AcceptanceTester($scenario);
 $I->wantTo('ensure that the 404 page works');
-$I->haveEnabledFilters();
 $I->amOnPage('/some/bad/url/that/will/never/exist');
-$I->seePageNotFound();
+$I->see('404 - Page Not Found');
 $I->dontSee('whoops');
