87152748b8949512cd35cd8cef50f24f2f796fda
Joe Ferguson <joe@joeferguson.me>
2015-08-19 16:02:20 -0500

    Fix invoices delete and edit tests

--- tests/acceptance/Invoices/InvoicesDeleteWorksCept.php
+++ tests/acceptance/Invoices/InvoicesDeleteWorksCept.php
@@ -3,6 +3,6 @@ $I = new AcceptanceTester($scenario);
 $I->wantTo('ensure that the invoice delete works');
 TestCommonAdmin::logMeIn($I);
 $I->amOnPage('/invoices/2');
-$I->click('#delete-1');
+$I->click('#delete-2');
 $I->amOnPage('/invoices');
 $I->dontSee('whoops');
--- tests/acceptance/Invoices/InvoicesEditWorksCept.php
+++ tests/acceptance/Invoices/InvoicesEditWorksCept.php
@@ -4,7 +4,7 @@ $I->wantTo('ensure that the invoice edit works');
 TestCommonAdmin::logMeIn($I);
 $I->amOnPage('/invoices/2');
 $I->click('//*[@id="edit-2"]');
-$I->amOnPage('/invoices/1/edit');
+$I->amOnPage('/invoices/2/edit');
 $I->see('Edit Invoice', 'h4');
 $I->fillField('due', '2004-06-07');
 $I->selectOption('owner_id', 'admin');
