8a33264d3cf22a8defa2b4a4419678e4826048a4
Joe Ferguson <joe@joeferguson.me>
2015-10-18 16:12:09 -0500

    cleanup customize.sh script

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -3,11 +3,6 @@ apt-get -y install phantomjs screen
 (crontab -l 2>/dev/null; echo "@reboot    screen -S server -d -m phantomjs --webdriver=4444") | crontab -
 echo "Added scree phantomjs to crontab"
 screen -S server -d -m phantomjs --webdriver=4444
-
-
-
-
-
 /usr/local/bin/composer self-update
 echo "" >> /home/vagrant/.bashrc
 echo "PATH=$PATH:/home/vagrant/simplelance.dev/vendor/bin" >> /home/vagrant/.bashrc
